e38f31e915b81c6db8b5881e3835bbeb9c06fbaf
Piotr Pokora <piotrek.pokora@gmail.com>
2009-12-21 16:40:34 +0000

    Take into account stringified "boolean" value when doing string to boolean conversion. Refs #1543

--- src/midgard.c
+++ src/midgard.c
@@ -82,6 +82,27 @@ static void __transform_string_to_boolean (const GValue *src_val, GValue *dest_v
 		return;
 	}
 
+	/* Stringified boolean */
+	if (g_str_equal (_str, "TRUE")) {
+		g_value_set_boolean (dest_val, TRUE);
+		return;
+	}
+
+	if (g_str_equal (_str, "true")) {
+		g_value_set_boolean (dest_val, TRUE);
+		return;
+	}
+
+	if (g_str_equal (_str, "FALSE")) {
+		g_value_set_boolean (dest_val, FALSE);
+		return;
+	}
+
+	if (g_str_equal (_str, "false")) {
+		g_value_set_boolean (dest_val, FALSE);
+		return;
+	}
+
 	gint i = atoi (_str);
 
 	if (i > 0)
