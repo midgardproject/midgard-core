b71d83202748e28c23f06c4a332c67928dbd242a
Alexey Zakhlestin <indeyets@gmail.com>
2010-01-19 12:44:23 +0000

    more visually appealing test-runner

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,39 +1,52 @@
 #!/bin/sh
 
 MIDGARD_TEST_DB="midgard_test"
+MIDGARD_TEST_DB_REPLICATOR_IMPORT="midgard_test_replicator_import"
+
 MIDAGRD_TEST_DB_USER="midgard_test"
 MIDGARD_TEST_DB_PASS="midgard_test"
 
 # ./autogen.sh
 
-echo "sudo mysqladmin create midgard_test"
+echo "Preparing temporary database…"
+echo "-> sudo mysqladmin create midgard_test"
 sudo mysql -e "CREATE DATABASE ${MIDGARD_TEST_DB} CHARACTER SET utf8";
-# Grant all privileges" 
-echo "GRANT all ON ${MIDGARD_TEST_DB}.*  to '${MIDGARD_TEST_DB_USER}'@'localhost' identified by '${MIDGARD_TEST_DB_PASS}'";
+echo "-> GRANT all ON ${MIDGARD_TEST_DB}.*  to '${MIDGARD_TEST_DB_USER}'@'localhost' identified by '${MIDGARD_TEST_DB_PASS}'";
 sudo mysql -e "GRANT all ON ${MIDGARD_TEST_DB}.*  to '${MIDGARD_TEST_DB_USER}'@'localhost' identified by '${MIDGARD_TEST_DB_PASS}'";
 sudo mysql -e " FLUSH PRIVILEGES";
 
-MIDGARD_TEST_DB_REPLICATOR_IMPORT="midgard_test_replicator_import"
 
-echo "sudo mysqladmin create midgard_test_replicator_import"
+echo "-> sudo mysqladmin create midgard_test_replicator_import"
 sudo mysql -e "CREATE DATABASE ${MIDGARD_TEST_DB_REPLICATOR_IMPORT} CHARACTER SET utf8";
-# Grant all privileges" 
-echo "GRANT all ON ${MIDGARD_TEST_DB_REPLICATOR_IMPORT}.*  to '${MIDGARD_TEST_DB_USER}'@'localhost' identified by '${MIDGARD_TEST_DB_PASS}'";
+echo "-> GRANT all ON ${MIDGARD_TEST_DB_REPLICATOR_IMPORT}.*  to '${MIDGARD_TEST_DB_USER}'@'localhost' identified by '${MIDGARD_TEST_DB_PASS}'";
 sudo mysql -e "GRANT all ON ${MIDGARD_TEST_DB_REPLICATOR_IMPORT}.*  to '${MIDGARD_TEST_DB_USER}'@'localhost' identified by '${MIDGARD_TEST_DB_PASS}'";
 sudo mysql -e " FLUSH PRIVILEGES";
 
 # Clean files
 rm -f midgard-test.html
 
+echo "\nRunning tests…\n"
 gtester -k -o midgard-test.xml \
 	./run-midgard-test-config \
 	./run-midgard-test-connection \
 	./run-midgard-test-object \
 	./run-midgard-test-tree \
 	./run-midgard-test-replicator \
-	# ./run-midgard-test-replicator-import \
-	./run-midgard-test-user
+	./run-midgard-test-user \
+	# ./run-midgard-test-replicator-import
+
+echo "\nCleanup…"
+echo "-> Droping database midgard_test"
+sudo mysqladmin -f drop midgard_test > /dev/null 2> /dev/null
+
+echo "-> Droping database midgard_test_replicator_import"
+sudo mysqladmin -f drop midgard_test_replicator_import > /dev/null 2> /dev/null
+
+
+echo "\nGenerating report… "
 gtester-report midgard-test.xml > midgard-test.html
+echo "-> DONE.    see midgard-test.html"
+
 
 # Those below might be enabled and used if one needs report file per test program 
 #gtester -k -o midgard-test-config.xml ./run-midgard-test-config
@@ -43,10 +56,4 @@ gtester-report midgard-test.xml > midgard-test.html
 #gtester -k -o midgard-test-object.xml ./run-midgard-test-object
 #gtester-report midgard-test-object.xml >> midgard-test.html
 #gtester-report midgard-test-object.xml > run-midgard-test-object.html
-
-echo "Droping database midgard_test"
-sudo mysqladmin -f drop midgard_test
-
-echo "Droping database midgard_test_replicator_import"
-sudo mysqladmin -f drop midgard_test_replicator_import
 
