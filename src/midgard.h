4cd627f5f3744b1bdcb2cc74edf7aa7d4803b7ca
Piotr Pokora <piotrek.pokora@gmail.com>
2010-03-31 08:37:21 +0000

    Include reflector_property header. Refs #1515

--- src/midgard.h
+++ src/midgard.h
@@ -58,6 +58,7 @@
 #include <midgard/midgard_query_simple_constraint.h>
 #include <midgard/midgard_query_constraint.h>
 #include <midgard/midgard_query_group_constraint.h>
+#include <midgard/midgard_reflector_property.h>
 
 #endif /* _MIDGARD_API_H_ */
 
