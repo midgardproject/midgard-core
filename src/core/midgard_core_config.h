639a7d612f62b048958a419988ada107a622d8b4
piotras <piotrek.pokora@gmail.com>
2010-09-13 19:32:55 +0200

    include local header

--- src/core/midgard_core_config.h
+++ src/core/midgard_core_config.h
@@ -17,8 +17,28 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#ifndef _MIDGARD_CORE_CONFIG_H_
+#define _MIDGARD_CORE_CONFIG_H_
+
 #include <glib-object.h>
 #include "midgard3.h"
+#include "midgard_local.h"
 
-gboolean
-midgard_core_config_read_file (MidgardConfig *config, const gchar *name, gboolean user, GError **error);
+typedef enum {
+	MIDGARD_CORE_DB_TYPE_MYSQL = 1,
+	MIDGARD_CORE_DB_TYPE_POSTGRES,
+	MIDGARD_CORE_DB_TYPE_FREETDS,
+	MIDGARD_CORE_DB_TYPE_SQLITE,
+	MIDGARD_CORE_DB_TYPE_ODBC,
+	MIDGARD_CORE_DB_TYPE_ORACLE
+} MidgardCoreDBType;
+
+gboolean	midgard_core_config_read_file		(MidgardConfig *config, const gchar *name, gboolean user, GError **error);
+gboolean        midgard_core_config_read_file_at_path	(MidgardConfig *self, const gchar *filepath, GError **error);
+gboolean        midgard_core_config_read_data           (MidgardConfig *self, const gchar *data, GError **error);
+gchar           **midgard_core_config_list_files        (gboolean user);
+gboolean        midgard_core_config_save_file           (MidgardConfig *self, const gchar *name, gboolean user, GError **error);
+gboolean        midgard_core_config_create_blobdir      (MidgardConfig *self);
+gchar 		*midgard_core_config_get_default_confdir();
+
+#endif /* _MIDGARD_CORE_CONFIG_H_ */
