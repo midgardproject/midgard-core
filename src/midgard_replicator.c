a4c364204f31aab0ea80e98b4b150ab5d22c6494
piotras <piotrek.pokora@gmail.com>
2011-02-14 19:27:18 +0100

    Free timestamps

--- src/midgard_replicator.c
+++ src/midgard_replicator.c
@@ -521,6 +521,9 @@ midgard_replicator_import_object (MidgardDBObject *object, gboolean force)
 		updated = midgard_timestamp_get_string_from_value ((const GValue *)&updated_timestamp);
 		dbupdated = midgard_timestamp_get_string_from_value ((const GValue *)&dbupdated_timestamp);
 
+		midgard_timestamp_free (updated_timestamp);
+		midgard_timestamp_free (dbupdated_timestamp);
+
 		/* We can use g_ascii_strcasecmp as it type cast every single 
 		 * pointer to integer and unsigned char returning substract result */		
 		gint datecmp;
