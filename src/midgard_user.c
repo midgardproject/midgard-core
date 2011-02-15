60fa4566a9c23c0bf20b53d03961dc230f631cab
piotras <piotrek.pokora@gmail.com>
2011-02-15 20:48:31 +0100

    Handle NULL value in warning log

--- src/midgard_user.c
+++ src/midgard_user.c
@@ -984,7 +984,7 @@ static void __set_from_sql(MidgardDBObject *object,
 	if (value && G_VALUE_HOLDS_STRING (value))
 		self->priv->password = g_value_dup_string (value);
 	else 
-		g_warning ("Invalid value type for user's password. Expected string, got %s", G_VALUE_TYPE_NAME (value));
+		g_warning ("Invalid value type for user's password. Expected string, got %s", value ? G_VALUE_TYPE_NAME (value) : "NULL");
 
 	/* PERSON GUID */
 	value = midgard_data_model_get_value_at_col_name (model, "person", row);
@@ -993,7 +993,7 @@ static void __set_from_sql(MidgardDBObject *object,
 		self->priv->person_guid = g_value_dup_string (value);
 	
 	} else {
-		g_warning ("Invalid value type for user's person guid. Expected string, got %s", G_VALUE_TYPE_NAME (value));
+		g_warning ("Invalid value type for user's person guid. Expected string, got %s", value ? G_VALUE_TYPE_NAME (value) : "NULL");
 	}
 
 	/* ACTIVE */
