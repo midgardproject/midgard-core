c246615d3717aff56ba0a8f35a29c68b6839f56d
piotras <piotrek.pokora@gmail.com>
2011-08-30 21:06:07 +0200

    Provide test schema. Test changes related to schema.

--- tests/midgard_test_types_and_extending.c
+++ tests/midgard_test_types_and_extending.c
@@ -32,19 +32,13 @@ midgard_test_types_and_extending_base_abstract_derived_type	(MidgardBaseAbstract
 	/* Check if type is abstract */
 	g_assert (G_TYPE_IS_ABSTRACT (abstract_type));
 
-	/* Find registered property */
-	GParamSpec *primary_type_spec = g_object_class_find_property (klass, "jcr-primaryType");
-	g_assert (primary_type_spec != NULL);
-
-	GParamSpec *mixin_spec = g_object_class_find_property (klass, "jcr-mixinTypes");
-	g_assert (mixin_spec != NULL);
-
 	/* Check parent type */
 	GType base_abstract_type = g_type_parent (abstract_type);
 	g_assert_cmpuint (base_abstract_type, ==, MIDGARD_TYPE_BASE_ABSTRACT);
 
 	g_assert_cmpuint (base_abstract_type, !=, G_TYPE_OBJECT);
 	g_assert_cmpuint (base_abstract_type, !=, MIDGARD_TYPE_DBOBJECT);
+	g_assert_cmpuint (base_abstract_type, !=, MIDGARD_TYPE_OBJECT);
 
 	g_type_class_unref (klass);
 }
@@ -52,32 +46,37 @@ midgard_test_types_and_extending_base_abstract_derived_type	(MidgardBaseAbstract
 void 	
 midgard_test_types_and_extending_base_abstract_derived_type_with_interfaces (MidgardBaseAbstractTest *mwct, gconstpointer data)
 {
-	GType nt_base_type = g_type_from_name ("nt_unstructured");
-	g_assert_cmpint (nt_base_type, !=, G_TYPE_NONE);
+	GType abstract_type = g_type_from_name ("AbstractTypeWithInterfaces");
+	g_assert_cmpint (abstract_type, !=, G_TYPE_NONE);
 
-	GObjectClass *klass = g_type_class_ref (nt_base_type);
+	GObjectClass *klass = g_type_class_ref (abstract_type);
 	g_assert (klass != NULL);
 
-	/* Check if type is abstract */
-	g_assert (G_TYPE_IS_ABSTRACT (nt_base_type) == FALSE);
+	/* Check implemented interfaces */
+	guint n_types;
+	guint i;
+	GType *ifaces = g_type_interfaces (abstract_type, &n_types);
+	g_assert_cmpuint (n_types, ==, 3);
 
-	/* Find registered property */
-	GParamSpec *primary_type_spec = g_object_class_find_property (klass, "jcr-primaryType");
-	g_assert (primary_type_spec != NULL);
+	for (i = 0; i < n_types; i++) {
+		g_warning ("TODO");
+	}
 
-	GParamSpec *mixin_spec = g_object_class_find_property (klass, "jcr-mixinTypes");
-	g_assert (mixin_spec != NULL);
+	/* Find properties from implemented interfaces */
+	GParamSpec *f_iface_f_prop = g_object_class_find_property (klass, "FirstIfaceFirstProperty");
+	g_assert (f_iface_f_prop != NULL);
 
-	/* Check parent type */
-	GType nt_unstructured_parent_type = g_type_parent (nt_base_type);
-	GType nt_base = g_type_from_name ("nt_base");
-	g_assert_cmpstr (g_type_name(nt_unstructured_parent_type), ==, g_type_name (nt_base));
-	g_assert_cmpuint (nt_unstructured_parent_type, ==, nt_base);
+	GParamSpec *f_iface_s_prop = g_object_class_find_property (klass, "FirstIfaceSecondProperty");
+	g_assert (f_iface_s_prop != NULL);
 
-	g_assert_cmpuint (nt_unstructured_parent_type, !=, G_TYPE_OBJECT);
-	g_assert_cmpuint (nt_unstructured_parent_type, !=, MIDGARD_TYPE_DBOBJECT);
+	GParamSpec *s_iface_f_prop = g_object_class_find_property (klass, "SecondIfaceFirstProperty");
+	g_assert (s_iface_f_prop != NULL);
+
+	GParamSpec *f_mixin_f_prop = g_object_class_find_property (klass, "FirstMixinFirstProperty");
+	g_assert (f_mixin_f_prop != NULL);
 
 	g_type_class_unref (klass);
+
 }
 
 void 	
