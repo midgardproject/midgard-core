b40a734cfc365c1ebc7cc22d8145f0e18d078622
piotras <piotrek.pokora@gmail.com>
2012-01-04 15:32:47 +0100

    Test storage create and update

--- tests/GIR/test_003_storage.py
+++ tests/GIR/test_003_storage.py
@@ -15,14 +15,24 @@ from gi.repository import GObject
 class TestStorage(unittest.TestCase):
   def testBaseStorage(self):
     mgd = TestConnection.openConnection()
+    types = []
+    self.getDBObjectDerived("MidgardDBObject", types)
+    for typename in types:
+      nsname = "Midgard." + typename[7:]
+      self.assertEqual(Midgard.Storage.exists(mgd, typename), False, "Storage for " + nsname + " shouldn't exist")
     self.assertTrue(Midgard.Storage.create_base_storage(mgd))
 
   def getDBObjectDerived(self, typename, types):
     children = GObject.type_children(typename)
     for gtype in children:
       childname = GObject.type_name(gtype)
-      types.append(childname)
       self.getDBObjectDerived(childname, types)
+      # FIXME, core should support some routines to check if type could have storage 
+      if (GObject.type_is_a(gtype, GObject.type_from_name("MidgardBaseInterface"))
+          or gtype.is_abstract()
+          or childname == "MidgardMetadata"):
+        continue
+      types.append(childname)
 
   def testCreateTypeStorage(self):
     mgd = TestConnection.openConnection()
@@ -30,18 +40,18 @@ class TestStorage(unittest.TestCase):
     self.getDBObjectDerived("MidgardDBObject", types)
     for typename in types:
       nsname = "Midgard." + typename[7:]
-      #gtype = gobject.type.from_name(typename)
-      if (GObject.type_is_a(GObject.type_from_name(typename), GObject.type_from_name("MidgardBaseInterface"))
-          or gobject.GObject.__gtype__.is_abstract("Midgard." + nsname)
-          or typename == "MidgardMetadata"):
-        continue
-      #print(inspect.isabstract(typename))
-      # FIXME, core should support some routines to check if type could have storage
-      Midgard.Storage.create(mgd, typename)
-      self.assertEqual(Midgard.Storage.exists(mgd, typename), True, "Failed to create " + nsname + " storage")
+      self.assertEqual(Midgard.Storage.create(mgd, typename), True, "Failed to create " + nsname + " storage")
+      self.assertEqual(Midgard.Storage.exists(mgd, typename), True, "Storage for " + nsname + " doesn't exists. Previosuly created.")
 
   def testUpdateTypeStorage(self):
-    self.assertEqual("ok", "NOT IMPLEMENTED")
+    mgd = TestConnection.openConnection()
+    types = []
+    self.getDBObjectDerived("MidgardDBObject", types)
+    for typename in types:
+      nsname = "Midgard." + typename[7:]
+      self.assertEqual(Midgard.Storage.exists(mgd, typename), True, "Storage for " + nsname + " doesn't exists. Already created.")
+      self.assertEqual(Midgard.Storage.update(mgd, typename), True, "Failed to update " + nsname + " storage")
+      self.assertEqual(Midgard.Storage.exists(mgd, typename), True, "Storage for " + nsname + " doesn't exists. Previosuly updated.")
 
 if __name__ == "__main__":
     unittest.main()
