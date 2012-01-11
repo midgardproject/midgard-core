c8bcfaad9e01930bedce931b56a2d32884c04f98
piotras <piotrek.pokora@gmail.com>
2012-01-11 18:37:24 +0100

    Install mysql, grant privileges

--- travis_midgard.sh
+++ travis_midgard.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Install Midgard2 library dependencies from OBS
-sudo apt-get install -y dbus libglib2.0-dev libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev valgrind python-gobject libgirepository1.0-dev libgirepository-1.0-1 libgda-4.0-mysql libgda-4.0-postgres gir1.2-freedesktop gobject-introspection gir1.2-gda-4.0 
+sudo apt-get install -y dbus libglib2.0-dev libgda-4.0-4 libgda-4.0-dev libxml2-dev libdbus-1-dev libdbus-glib-1-dev valgrind python-gobject libgirepository1.0-dev libgirepository-1.0-1 libgda-4.0-mysql libgda-4.0-postgres gir1.2-freedesktop gobject-introspection gir1.2-gda-4.0 mysql-server mysql-client 
 
 ./autogen.sh --prefix=/usr 
 make
 sudo make install
 
 mysql -e 'create database test_gir;'
+mysql -e "GRANT all ON test_gir.*  to 'midgard'@'localhost' identified by 'midgard'";
+mysql -e " FLUSH PRIVILEGES";
 psql -c 'create database test_gir;' -U postgres
