effaf054849344b5f6d7418fa19e0b7646798d81
piotras <piotrek.pokora@gmail.com>
2012-01-12 16:35:49 +0100

    Use midgard_snippet and midgard_snippetdir to test parent, up properties and list_children

--- tests/GIR/test_008_reflector_object.py
+++ tests/GIR/test_008_reflector_object.py
@@ -13,6 +13,8 @@ class TestReflectorObject(unittest.TestCase):
   person_type = "midgard_person"
   file_type = "nt_file"
   resource_type = "nt_resource"
+  snippet_type = "midgard_snippet"
+  snippetdir_type = "midgard_snippetdir"
 
   def setUp(self):
     if self.mgd == None:
@@ -27,31 +29,40 @@ class TestReflectorObject(unittest.TestCase):
     self.assertEqual(Midgard.ReflectorObject.get_property_up(self.person_type), None)
     self.assertEqual(Midgard.ReflectorObject.get_property_up(self.file_type), None)
     self.assertEqual(Midgard.ReflectorObject.get_property_up(self.resource_type), None)
+    self.assertEqual(Midgard.ReflectorObject.get_property_up(self.snippetdir_type), "up")
 
   def testPropertyParent(self):
     self.assertEqual(Midgard.ReflectorObject.get_property_parent(self.person_type), None)
     self.assertEqual(Midgard.ReflectorObject.get_property_parent(self.file_type), None)
     self.assertEqual(Midgard.ReflectorObject.get_property_parent(self.resource_type), None)
+    self.assertEqual(Midgard.ReflectorObject.get_property_parent(self.snippet_type), "snippetdir")
 
   def testPropertyUnique(self):
     self.assertEqual(Midgard.ReflectorObject.get_property_unique(self.person_type), "firstname")
     self.assertEqual(Midgard.ReflectorObject.get_property_unique(self.file_type), None)
     self.assertEqual(Midgard.ReflectorObject.get_property_unique(self.resource_type), None)
+    self.assertEqual(Midgard.ReflectorObject.get_property_unique(self.snippet_type), "name")
+    self.assertEqual(Midgard.ReflectorObject.get_property_unique(self.snippetdir_type), "name")
 
   def testListChildren(self):
     self.assertEqual(len(Midgard.ReflectorObject.list_children(self.person_type)), 0)
     self.assertEqual(len(Midgard.ReflectorObject.list_children(self.file_type)), 0)
     self.assertEqual(len(Midgard.ReflectorObject.list_children(self.resource_type)), 0)
+    self.assertEqual(len(Midgard.ReflectorObject.list_children(self.snippetdir_type)), 1)
+    self.assertIn("midgard_snippet", Midgard.ReflectorObject.list_children(self.snippetdir_type))
 
   def testHasMetadata(self):
     self.assertTrue(Midgard.ReflectorObject.has_metadata_class(self.person_type))
     self.assertFalse(Midgard.ReflectorObject.has_metadata_class(self.file_type))
     self.assertFalse(Midgard.ReflectorObject.has_metadata_class(self.resource_type))
- 
+    self.assertTrue(Midgard.ReflectorObject.has_metadata_class(self.snippet_type))
+    self.assertTrue(Midgard.ReflectorObject.has_metadata_class(self.snippetdir_type))
+
   def testGetMetadataClass(self):
     self.assertEqual(Midgard.ReflectorObject.get_metadata_class(self.person_type), "MidgardMetadata")
     self.assertIsNone(Midgard.ReflectorObject.get_metadata_class(self.file_type))
     self.assertIsNone(Midgard.ReflectorObject.get_metadata_class(self.resource_type))
+    self.assertEqual(Midgard.ReflectorObject.get_metadata_class(self.snippet_type), "MidgardMetadata")
 
 if __name__ == "__main__":
     unittest.main()
