84d8758f35ead33363a748b26e7909d1e14ddfd8
piotras <piotrek.pokora@gmail.com>
2012-01-23 20:14:23 +0100

    Typo fix. Refs gh-25

--- src/midgard_query_result.h
+++ src/midgard_query_result.h
@@ -26,7 +26,7 @@
 G_BEGIN_DECLS
 
 /* convention macros */
-#define MIDGARD_TYPE_QUERY_RESULT (midgard_query_select_get_type()) 
+#define MIDGARD_TYPE_QUERY_RESULT (midgard_query_result_get_type()) 
 #define MIDGARD_QUERY_RESULT(object)  (G_TYPE_CHECK_INSTANCE_CAST ((object),MIDGARD_TYPE_QUERY_RESULT, MidgardQueryResult))
 #define MIDGARD_IS_QUERY_RESULT(object) (G_TYPE_CHECK_INSTANCE_TYPE ((object), MIDGARD_TYPE_QUERY_RESULT))
 #define MIDGARD_QUERY_RESULT_GET_INTERFACE(inst) (G_TYPE_INSTANCE_GET_INTERFACE ((inst), MIDGARD_TYPE_QUERY_RESULT, MidgardQueryResultIFace))
