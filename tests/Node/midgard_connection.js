44cb3d93f67847d489c92af2ce9b8fd930f68e72
piotras <piotrek.pokora@gmail.com>
2012-04-27 15:10:36 +0200

    Initial

---
+++ tests/Node/midgard_connection.js
@@ -0,0 +1,19 @@
+
+var vows   = require('vows'),
+    assert = require('assert');
+
+var gir = require('gir');
+gir.init();
+
+var Midgard, gir, config, mgd;
+Midgard = gir.load('Midgard');
+Midgard.init();
+
+config = new Midgard.Config();
+config.__set_property__("dbdir", "/tmp");
+config.__set_property__("dbtype", "SQLite");
+config.__set_property__("database", "node_gir");
+
+mgd = new Midgard.Connection();
+
+exports.cnc = mgd;
