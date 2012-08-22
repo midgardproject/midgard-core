7d8e0e0a7426aec1235a70fa3daefda1ac88e19c
piotras <piotrek.pokora@gmail.com>
2012-08-22 11:44:28 +0200

    Initial. Refs gh-168

---
+++ tests/GIR/bookstorequery.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import os
+import sys
+
+from gi.repository import Midgard, GObject, GLib
+
+class BookStoreQuery():
+
+  @staticmethod
+  def findByName(mgd, name):
+    storage = Midgard.QueryStorage(dbclass = "gir_test_book_store")
+    qs = Midgard.QuerySelect(connection = mgd, storage = storage)
+    qs.set_constraint(
+      Midgard.QueryConstraint(
+        property = Midgard.QueryProperty(property = "name"),
+        operator = "=",
+        holder = Midgard.QueryValue.create_with_value(name)
+      )
+    )
+    qs.execute()
+    return qs.list_objects()
